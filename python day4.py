fe07baa2d335a962bded346781058c24d4aed6b5
madhumunagala04 <68542991+madhumunagala04@users.noreply.github.com>
2020-07-23 17:18:25 +0530

    Add files via upload

---
+++ python day4.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[29]:
+
+
+str="success is not a destination,success is a journey"
+
+
+# In[36]:
+
+
+str.rfind("success")
+
+
+# is upper
+
+# In[42]:
+
+
+str.isupper()
+
+
+# is lower
+
+# In[41]:
+
+
+str.islower()
+
+
+# In[43]:
+
+
+str2="Yaswanth"
+
+
+# In[44]:
+
+
+str2.islower()
+
+
+# In[ ]:
+
+
+
+
